9479e5bb5313abd59bfd0ed084048be9fd4b6a88
preethampotu <preetham.reddy16@gmail.com>
2019-10-06 16:36:01 -0500

    Update reducer.py in maxTripDuration

--- minTripDuration/reducer.py
+++ minTripDuration/reducer.py
@@ -4,8 +4,16 @@ y = open("reduceroutput.txt","w")
 k = []
 for line in x:
     data = line.strip().split("\t")
+    usertype,tripduration = data
     if(data[0] == "Subscriber"):
             k.append(int(float(data[1])))
-y.write("{0}\t{1}\n".format("Subscriber",min(k)))            
-print(min(k))
+def minTripDuration(l):
+    low = l[0]
+    for find_min in l:
+        if low > find_min:
+            low = find_min
+    return low  
+print(minTripDuration(k))      
+y.write("{0}\t{1}\n".format(usertype,minTripDuration(k)))            
+
         
