a20273570129a3d7b63dd3c5f8b8a22b61123488
preethampotu <preetham.reddy16@gmail.com>
2019-10-06 16:32:56 -0500

    Updated reducer.py of maxTripDuration/reducer.py

--- maxTripDuration/reducer.py
+++ maxTripDuration/reducer.py
@@ -1,10 +1,21 @@
 x = open("PreethamMapperOutput.txt",'r')
-y=open("PreethamReducerOutput.txt",'w')
+#open file, read-only raw data
+y = open("PreethamReducerOutput.txt",'w')
+# open file, write - just our key, value pairs
 s = []
 for line in x:
+    # parses for line starting with quotes
     data = line.strip().split("\t")
+    # splits the line separated by comma
     if(data[0] == "Subscriber"):
              s.append(int(data[1]))
 
-y.write("{0}\t{1}\n".format("Subscriber",max(s)))
-print(max(s))
+def maxtripduration(l):
+    high = l[0]
+    for find_max in l:
+        if high < find_max:
+            high = find_max
+    return high
+print(maxtripduration(s))
+y.write("{0}\t{1}\n".format("Subscriber",maxtripduration(s)))
+
