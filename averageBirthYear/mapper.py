25e5f400a452b60dc31ca7887683865d359388cc
Boppidi Jyoshna <s533706@nwmissouri.edu>
2019-10-12 23:16:53 -0500

    added comments in average birth year

--- averageBirthYear/mapper.py
+++ averageBirthYear/mapper.py
@@ -1,10 +1,16 @@
-input_mapper = open("../data/Divvy_Trips_2018_Q1.txt","r") 
-output_mapper = open("output_mapper.txt","w")
+input_mapper = open("../data/Divvy_Trips_2018_Q1.txt","r") # open file, read-only raw data
+output_mapper = open("output_mapper.txt","w")# open file, write - just our key, value pairs
+# For loop for reading each line from the text file
 for line in input_mapper:
+       # splits the line separated by comma
     data = line.strip().split(',')
+     # Checking the length of array
     if (len(data) == 12):
+        # assign list to each variable
         trip_id, start_time, end_time, bikeid, tripduration, from_station_id, from_station_name, to_station_id, to_station_name, usertype, gender, birthyear = data
         output_mapper.write(usertype+","+birthyear+"\n")
+                # printing usertype and birthyear to console input
         print(usertype+","+birthyear+"\n")
+        #closing files
 input_mapper.close()
 output_mapper.close()
--- averageBirthYear/reducer.py
+++ averageBirthYear/reducer.py
@@ -1,12 +1,13 @@
-input_reducer = open("output_mapper.txt","r")
-output_reducer = open("output_reducer.txt", "w")
-lines = input_reducer.readlines()
-lines.sort()
+input_reducer = open("output_mapper.txt","r")# open file, read-only raw data
+output_reducer = open("output_reducer.txt", "w") # open file, writes final output value
+lines = input_reducer.readlines()#reading each line from data and storing it in array
+lines.sort()#sorting the array
 total = 0
 length = 0
 for line in lines:
+     # splits the line separated by comma
     data = line.strip().split(',')
-    usertype = data[0]
+    usertype = data[0]#Assigning data to sex
     try:
         birthyear = float(data[1])
         length = length + 1
@@ -14,9 +15,12 @@ for line in lines:
     except ValueError:
         continue
 
-
+#finding average
 average = total/length
+# writes usertype and average files into new output_reducer.txt file
 output_reducer.write(usertype+","+str(average)+"\n")
+#writing usertype and average to console input
 print(usertype+","+str(average)+"\n")
+#closing files
 input_reducer.close()
 output_reducer.close()
