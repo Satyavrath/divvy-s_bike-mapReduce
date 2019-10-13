d13de1daa3f01c581473d5004b18a146d6880cfd
Akhila Gandra <akhila.gandra08@gmail.com>
2019-10-12 23:42:24 -0500

    Added comments in the mapper.py & reducer.py files

--- minTripDuration/mapper.py
+++ minTripDuration/mapper.py
@@ -1,7 +1,8 @@
 import re
 x = open("Divvy_Trips_2018_Q1.txt", 'r')
-
+# open and read contents of this file
 y = open("Mapperoutput.txt","w")
+# open and write into this file
 for line in x:
    a = re.search('.*(\".*\").*',line)
    if a:
@@ -15,5 +16,7 @@ for line in x:
            trip_id,start_time,end_time,bikeid,tripduration,from_station_id,from_station_name,to_station_id,to_station_name,usertype,gender,birthyear = data
            y.write("{0}\t{1}\n".format(usertype,tripduration))
            print("{0}\t{1}\n".format(usertype,tripduration))
+           # print("{0}\t{1}\n".format(usertype,(tripduration)))
 x.close()
 y.close()
+# Closing both the read only and write files.
--- minTripDuration/reducer.py
+++ minTripDuration/reducer.py
@@ -1,9 +1,13 @@
 x = open("Mapperoutput.txt","r")
+#open file, read-only raw data
 y = open("reduceroutput.txt","w")
+# open file, write - just our key, value pairs
 
 k = []
 for line in x:
+    # parses for line starting with quotes
     data = line.strip().split("\t")
+    # splits the line separated by comma
     usertype,tripduration = data
     if(data[0] == "Subscriber"):
             k.append(int(float(data[1])))
